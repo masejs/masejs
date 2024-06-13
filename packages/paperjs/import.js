295be5076f55ecf466ac85a8ad8a1b64568d1a47
Brick_Wall <118539051+GreenestGoat@users.noreply.github.com>
2024-06-13 10:01:47 +0000

    added support for multiple classes

--- packages/paperjs/import.js
+++ packages/paperjs/import.js
@@ -1,56 +1,58 @@
 export class PaperJSInterpreter {
-    static interpret(paperjs) {
-      for (const tag in paperjs) {
-        const element = this.createElement(tag, paperjs[tag]);
-        this.applyElementOptions(element, paperjs[tag]);
-        document.body.appendChild(element);
-      }
+  static interpret(paperjs) {
+    for (const tag in paperjs) {
+      const element = this.createElement(tag, paperjs[tag]);
+      this.applyElementOptions(element, paperjs[tag]);
+      document.body.appendChild(element);
     }
-  
-    static createElement(tag, options) {
-      if (typeof options === 'string') {
-        const element = document.createElement('custom-element');
-        element.innerHTML = options;
-        return element;
-      } else {
-        const element = document.createElement(tag);
-        return element;
-      }
+  }
+
+  static createElement(tag, options) {
+    if (typeof options === 'string') {
+      const element = document.createElement('custom-element');
+      element.innerHTML = options;
+      return element;
+    } else {
+      const element = document.createElement(tag);
+      return element;
     }
-  
-    static applyElementOptions(element, options) {
-      if (typeof options === 'object') {
-        for (const option in options) {
-          if (option === 'value') {
-            element.textContent = options[option];
-          } else if (option === 'styles') {
-            this.applyStyles(element, options[option]);
-          } else if (option === 'class') {
-            element.classList.add(options[option]);
-          } else if (option === 'id') {
-            element.id = options[option];
-          } else if (Array.isArray(options[option])) {
-            for (const item of options[option]) {
-              const nestedElement = this.createElement(option, item);
-              this.applyElementOptions(nestedElement, item);
-              element.appendChild(nestedElement);
-            }
-          } else {
-            element.setAttribute(option, options[option]);
-          }
+  }
+
+  static applyElementOptions(element, options) {
+  if (typeof options === 'object') {
+    for (const option in options) {
+      if (option === 'value') {
+        element.textContent = options[option];
+      } else if (option === 'styles') {
+        this.applyStyles(element, options[option]);
+      } else if (option === 'class') {
+        const classes = options[option].split(' ');
+        classes.forEach(cls => element.classList.add(cls));
+      } else if (option === 'id') {
+        const ids = options[option].split(' ');
+        ids.forEach(id => element.id += ` ${id}`);
+      } else if (Array.isArray(options[option])) {
+        for (const item of options[option]) {
+          const nestedElement = this.createElement(option, item);
+          this.applyElementOptions(nestedElement, item);
+          element.appendChild(nestedElement);
         }
+      } else {
+        element.setAttribute(option, options[option]);
       }
     }
-  
-    static applyStyles(element, styles) {
-      if (styles) {
-        const styleArray = styles.split(';');
-        for (const style of styleArray) {
-          if (style.trim() !== '') {
-            const [property, value] = style.split(':');
-            element.style[property.trim()] = value.trim();
-          }
+  }
+}
+
+  static applyStyles(element, styles) {
+    if (styles) {
+      const styleArray = styles.split(';');
+      for (const style of styleArray) {
+        if (style.trim() !== '') {
+          const [property, value] = style.split(':');
+          element.style[property.trim()] = value.trim();
         }
       }
     }
   }
+}
